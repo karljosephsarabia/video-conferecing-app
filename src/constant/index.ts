e491b919a7ca2706aee18a991bfac26a544a588b
Karl Joseph Sarabia <sarabia.karljoseph@gmail.com>
2025-01-15 14:09:56 +0800

    Added constant for sidebar links on constant folder

---
+++ src/constant/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks = [
+    {
+        name: 'Home',
+        route: '/',
+        imgUrl: '/icons/home.svg'
+    },
+    {
+        name: 'Upcoming',
+        route: '/upcoming',
+        imgUrl: '/icons/upcoming.svg'
+    },
+    {
+        name: 'Previous',
+        route: '/previous',
+        imgUrl: '/icons/previous.svg'
+    },
+    {
+        name: 'Recordings',
+        route: '/recording',
+        imgUrl: '/icons/recording.svg'
+    },
+    {
+        name: 'Personal Room',
+        route: '/personal-room',
+        imgUrl: '/icons/plus.svg'
+    },
+]

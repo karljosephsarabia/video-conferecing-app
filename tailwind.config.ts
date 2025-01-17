294b1f12dcfa501955fa40b77ce873e8b183f839
Karl Joseph Sarabia <sarabia.karljoseph@gmail.com>
2025-01-17 22:13:21 +0800

    Add color theme on tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,40 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	darkMode: ["class"],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				dark: {
+					1: '#1C1F2E',
+					2: '#161925'
+				},
+				blue: {
+					1: '#0E78F9'
+				},
+				sky: {
+					1: '#c9ddff'
+				},
+				orange: {
+					1: '#FF742E'
+				},
+				purple: {
+					1: '#830EF9'
+				},
+				yellow: {
+					1: '#F9A90E'
+				}
+			},
+			backgroundImage: {
+				hero: "url('/images/hero-background.png')",
+
+			}
+		}
+	},
+	plugins: [require("tailwindcss-animate")],
 } satisfies Config;

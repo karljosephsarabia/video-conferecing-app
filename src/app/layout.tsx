141f0c08733f3d85fb26f92f21ee825239a6f1f2
Karl Joseph Sarabia <sarabia.karljoseph@gmail.com>
2025-01-17 22:15:58 +0800

    Integrate clerkprovider, toaster on global layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/toaster";
+import '@stream-io/video-react-sdk/dist/css/styles.css';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +26,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-dark-2`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        layout: {
+          logoImageUrl: '/icons/yoom-logo.svg',
+          socialButtonsVariant: 'iconButton'
+        },
+        variables: {
+          colorText: '#fff',
+          colorPrimary: '#0E78F9',
+          colorBackground: '#1c1f2e',
+          colorInputBackground: '#252a41',
+          colorInputText: '#fff'
+        }
+      }}>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased bg-dark-2`}
+        >
+          {children}
+          <Toaster />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
